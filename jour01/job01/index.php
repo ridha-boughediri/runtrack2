a727eb02d3897bbaec3a22bd981cd212d9892551
ridha-boughediri <ridh.bouhjediri@laplateforme.io>
2021-11-02 12:30:20 +0100

    comment

---
+++ jour01/job01/index.php
@@ -0,0 +1,14 @@
+<?php
+$str = "Vive";
+$str1 = "Laplateforme";
+$str2 = "!";
+echo $str.$str1.$str2;
+$val = 6;
+echo $val;
+$val = $val + 4;
+echo $val;
+$myBool = true;
+echo $myBool;
+$myBool = false;
+echo $myBool;
+?>
